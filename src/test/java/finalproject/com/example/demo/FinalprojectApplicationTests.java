421a3b67f72098021c4522eb10ec101d8ac2e90f
Julie Folkerts <JulieVMF@gmail.com>
2025-12-25 07:33:07 +0000

    set up final project

---
+++ src/test/java/finalproject/com/example/demo/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
